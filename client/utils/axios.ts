f0013aad12ead13e96a8d51a3dd1771658d8df24
Ratul <rafiratul01@gmail.com>
2022-06-20 02:55:31 +0600

    making it good bro

--- client/utils/axios.ts
+++ client/utils/axios.ts
@@ -2,8 +2,8 @@ import axios from "axios";
 
 
 const instance = axios.create({
-    // baseURL:`https://ave-dating-site.herokuapp.com`,
-    baseURL:`http://localhost:8000/`,
+    baseURL:`https://ave-dating-site.herokuapp.com`,
+    // baseURL:`http://localhost:8000/`,
     timeout:5000,
 });
 

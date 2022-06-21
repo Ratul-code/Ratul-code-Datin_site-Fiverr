aecb28b39b3f8e2b325c5e65472f403aaa8e0105
Ratul <rafiratul01@gmail.com>
2022-06-21 13:51:47 +0600

    maing it better

--- client/pages/membership.tsx
+++ client/pages/membership.tsx
@@ -1,13 +1,19 @@
 import React, { useEffect, useState } from 'react'
 import MainLayout from '../components/MainLayout'
 import Pricing from '../components/Pricing'
+import { useAppSelector } from '../redux/hooks'
 import instance from '../utils/axios'
 
 // const Membership = ({prices=null}:any) => {
 const Membership = () => {
   const [prices,setPrices] = useState<any>();
+  const {user} = useAppSelector(state=>state);
   const fetchUserplan = async ()=>{
-    const {data} = await instance.get("/subs/prices");
+    const {data} = await instance.get("/subs/prices",{
+      headers:{
+        Authorization:`${user.token}`
+      }
+    });
     setPrices(data);
   }
   useEffect(()=>{

c1260fa5c05c16ddc2f991aeac05236535ed9722
Ratul <rafiratul01@gmail.com>
2022-06-21 13:43:27 +0600

    maing it better

--- client/pages/membership.tsx
+++ client/pages/membership.tsx
@@ -1,10 +1,18 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import MainLayout from '../components/MainLayout'
 import Pricing from '../components/Pricing'
-import { useAppSelector } from '../redux/hooks'
 import instance from '../utils/axios'
 
-const Membership = ({prices=null}:any) => {
+// const Membership = ({prices=null}:any) => {
+const Membership = () => {
+  const [prices,setPrices] = useState<any>();
+  const fetchUserplan = async ()=>{
+    const {data} = await instance.get("/subs/prices");
+    setPrices(data);
+  }
+  useEffect(()=>{
+    fetchUserplan();
+  },[])
   return (
     <MainLayout>
         {prices && <Pricing prices={prices}/>}
@@ -15,9 +23,9 @@ const Membership = ({prices=null}:any) => {
 export default Membership
 
 
-export async function getStaticProps() {
-  const {data} = await instance.get("/subs/prices");
-  return {
-    props: {prices:data}, 
-  }
-}
+// export async function getStaticProps() {
+//   const {data} = await instance.get("/subs/prices");
+//   return {
+//     props: {prices:data}, 
+//   }
+// }
